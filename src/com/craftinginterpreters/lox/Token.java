9543660e78aa3b597a9c7308d136aabf432723fd
szng <shezeneng@qq.com>
2024-03-21 23:25:06 +0800

    Add scanner for Lox

---
+++ src/com/craftinginterpreters/lox/Token.java
@@ -0,0 +1,19 @@
+package com.craftinginterpreters.lox;
+
+class Token {
+    final TokenType type;
+    final String lexeme;    // 词素
+    final Object literal;
+    final int line; // 行号
+
+    Token(TokenType type, String lexeme, Object literal, int line) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    public String toString() {
+        return type + " " + lexeme + " " + literal + " " + line;
+    }
+}
---
+++ src/com/craftinginterpreters/lox/TokenType.java
@@ -0,0 +1,22 @@
+package com.craftinginterpreters.lox;
+
+enum TokenType {
+    // 单字符标识
+    LEFT_PAREN, RIGHT_PAREN, LET_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // 单字符或双字符标识
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // 字面量
+    IDENTIFIER, STRING, NUMBER,
+
+    // 关键字
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
+}
