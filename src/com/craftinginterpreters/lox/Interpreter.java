816fd17f08562af828699c73c63a9651eaddcbc2
szng <shezeneng@qq.com>
2024-03-24 10:32:10 +0800

    Add Interpreter for Lox
    evaluate AST to get value of expressions and handle runtime error using exception.
    
    A bug was found in REPL. In java, two output streams, out and err, have different buffers and cause the order issue. // Do not use err to output.

---
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -0,0 +1,161 @@
+package com.craftinginterpreters.lox;
+
+class Interpreter implements Expr.Visitor<Object> {
+    void interpreter(Expr expression) {
+        try {
+            Object value = evaluate(expression);
+            System.out.println(stringify(value));
+        } catch (RuntimeError error) {
+            Lox.runtimeError(error);
+        }
+    }
+
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case GREATER:
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left > (double) right;
+            case GREATER_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left >= (double) right;
+            case LESS:
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left < (double) right;
+            case LESS_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left <= (double) right;
+            case BANG_EQUAL:
+                return !isEqual(left, right);
+            case EQUAL_EQUAL:
+                return isEqual(left, right);
+            case MINUS:
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left - (double) right;
+            case PLUS:
+                if (left instanceof Double && right instanceof Double) {
+                    return (double) left + (double) right;
+                }
+                if (left instanceof String && right instanceof String) {
+                    return (String) left + (String) right;
+                }
+                // 字符串和数字相加
+                // todo 区分int和double
+                if (left instanceof String || right instanceof String) {
+                    return left.toString() + right.toString();
+                }
+
+                throw new RuntimeError(expr.operator,
+                        "Operands must be number or two strings");
+            case SLASH:
+                checkNumberOperands(expr.operator, left, right);
+                if (right.equals(0.0)) {
+                    throw new RuntimeError(expr.operator, "Divisor can't be zero.");
+                }
+                return (double) left / (double) right;
+            case STAR:
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left * (double) right;
+        }
+
+        return null;
+    }
+
+    private void checkNumberOperands(Token operator,
+                                     Object left,
+                                     Object right) {
+        if (left instanceof Double && right instanceof Double) {
+            return;
+        }
+
+        throw new RuntimeError(operator, "Operands must be numbers.");
+    }
+
+    private boolean isEqual(Object a, Object b) {
+        if (a == null && b == null) {
+            return true;
+        }
+        if (a == null) {
+            return false;
+        }
+
+        return a.equals(b);
+    }
+
+    private String stringify(Object object) {
+        if (object == null) {
+            return "nil";
+        }
+
+        if (object instanceof Double) {
+            String text = object.toString();
+            if (text.endsWith(".0")) {
+                text = text.substring(0, text.length() - 2);
+            }
+            return text;
+        }
+
+        return object.toString();
+    }
+
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr) {
+        return evaluate(expr.expression);
+    }
+
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return expr.value;
+    }
+
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case MINUS:
+                checkNumberOperand(expr.operator, right);
+                return -(double)right;
+            case BANG:
+                return !isTruthy(right);
+        }
+
+        return null;
+    }
+
+    private void checkNumberOperand(Token operator, Object operand) {
+        if (operand instanceof Double) {
+            return;
+        }
+        throw new RuntimeError(operator, "Operand must be a number.");
+    }
+
+    private boolean isTruthy(Object object) {
+        if (object == null) {
+            return false;
+        }
+        if (object instanceof Boolean) {
+            return (boolean) object;
+        }
+        return true;
+    }
+
+    @Override
+    public Object visitTernaryExpr(Expr.Ternary expr) {
+        Object cond = evaluate(expr.condition);
+
+        if (isTruthy(cond)) {
+            return evaluate(expr.left);
+        } else {
+            return evaluate(expr.right);
+        }
+    }
+
+}
