b963c122ab49cce9856f601dfa4e4bdfdbeb1680
szng <shezeneng@qq.com>
2024-05-22 21:02:06 +0800

    Add virtual machine for clox
    Add VM in vm.c and VM can interpret op codes of a chunk.
    Add a stack in the vm which stores values of load op.
    Add some basic arithmetic op codes.

--- clox/main.c
+++ clox/main.c
@@ -1,16 +1,28 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char *argv[])
 {
+    initVM();
+
     Chunk chunk;
     initChunk(&chunk);
 
     writeConstant(&chunk, 42, 1);
+    writeConstant(&chunk, 2, 1);
+
+    writeChunk(&chunk, OP_ADD, 1);
+
+    writeConstant(&chunk, 11, 1);
+    writeChunk(&chunk, OP_DIVIDE, 2);
+    writeChunk(&chunk, OP_NEGATE, 2);
     writeChunk(&chunk, OP_RETURN, 2);
 
     disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
     return 0;
 }
