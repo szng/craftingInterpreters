ddeccbf1b00fca71335c95a0fb877f89ef857c7e
szng <shezeneng@qq.com>
2024-05-26 12:12:15 +0800

    Add scanner for clox
    - Add a pipeline to scan, compile and execute codes by chain calling
    (execute call compile function, compile function call scan function).
    - Add two methods to run interpreter including repl and file.

--- clox/vm.c
+++ clox/vm.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "common.h"
+#include "compiler.h"
 #include "vm.h"
 #include "debug.h"
 
@@ -101,9 +102,8 @@ static InterpretResult run()
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk *chunk)
+InterpretResult interpret(const char *source)
 {
-    vm.chunk = chunk;
-    vm.ip = vm.chunk->code;
-    return run();
+    compile(source);
+    return INTERPRET_OK;
 }
