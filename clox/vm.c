a5d5d39ba803e3709a7cc6592a5043f394b72199
szng <shezeneng@qq.com>
2024-06-08 18:48:36 +0800

    Add string to clox
    Implement string object inheriting object using struct.
    Also, add + operator and free function of string.

--- clox/vm.c
+++ clox/vm.c
@@ -1,9 +1,12 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "common.h"
 #include "compiler.h"
 #include "vm.h"
+#include "object.h"
+#include "memory.h"
 #include "debug.h"
 
 VM vm;
@@ -30,10 +33,12 @@ static void runtimeError(const char *format, ...)
 void initVM()
 {
     resetStack();
+    vm.objects = NULL;
 }
 
 void freeVM()
 {
+    freeObjects();
 }
 
 void push(Value value)
@@ -58,6 +63,21 @@ static bool isFalsey(Value value)
     return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
 }
 
+static void concatenate()
+{
+    ObjString *b = AS_STRING(pop());
+    ObjString *a = AS_STRING(pop());
+
+    int length = a->length + b->length;
+    char *chars = ALLOCATE(char, length + 1);
+    memcpy(chars, a->chars, a->length);
+    memcpy(chars + a->length, b->chars, b->length);
+    chars[length] = '\0';
+
+    ObjString *result = takeString(chars, length);
+    push(OBJ_VAL(result));
+}
+
 static InterpretResult run()
 {
 #define READ_BYTE() (*vm.ip++)
@@ -65,7 +85,7 @@ static InterpretResult run()
 #define BINARY_OP(valueType, op)                        \
     do                                                  \
     {                                                   \
-        if (!IS_NUMBER(peek(0)) && !IS_NUMBER(peek(0))) \
+        if (!IS_NUMBER(peek(0)) && !IS_NUMBER(peek(1))) \
         {                                               \
             runtimeError("Operands must be numbers.");  \
             return INTERPRET_RUNTIME_ERROR;             \
@@ -104,11 +124,11 @@ static InterpretResult run()
             // todo
             break;
         }
-        case OP_ADD:
-            BINARY_OP(NUMBER_VAL, +);
-            break;
         case OP_NIL:
             push(NIL_VAL);
+            break;
+        case OP_TRUE:
+            push(BOOL_VAL(true));
             break;
         case OP_FALSE:
             push(BOOL_VAL(false));
@@ -126,8 +146,23 @@ static InterpretResult run()
         case OP_LESS:
             BINARY_OP(BOOL_VAL, <);
             break;
-        case OP_TRUE:
-            push(BOOL_VAL(true));
+        case OP_ADD:
+            if (IS_STRING(peek(0)) && IS_STRING(peek(1)))
+            {
+                concatenate();
+            }
+            else if (IS_NUMBER(peek(0)) && IS_NUMBER(peek(1)))
+            {
+                double b = AS_NUMBER(pop());
+                double a = AS_NUMBER(pop());
+                push(NUMBER_VAL(a + b));
+            }
+            else
+            {
+                runtimeError(
+                    "Operands must be two numbers or two strings.");
+                return INTERPRET_RUNTIME_ERROR;
+            }
             break;
         case OP_SUBTRACT:
             BINARY_OP(NUMBER_VAL, -);
