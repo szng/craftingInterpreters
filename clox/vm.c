7db3141fd7a3a90bf8e018964e475ba43cf48d7f
szng <shezeneng@qq.com>
2024-08-19 00:36:44 +0800

    Add globale variable value assignment

--- clox/vm.c
+++ clox/vm.c
@@ -146,7 +146,7 @@ static InterpretResult run()
         {
             ObjString *name = READ_STRING();
             Value value;
-            if (!tableGet(&vm.globals, name , &value))
+            if (!tableGet(&vm.globals, name, &value))
             {
                 runtimeError("Undefine variable '%s'.", name->chars);
                 return INTERPRET_RUNTIME_ERROR;
@@ -159,6 +159,17 @@ static InterpretResult run()
             ObjString *name = READ_STRING();
             tableSet(&vm.globals, name, peek(0));
             pop();
+            break;
+        }
+        case OP_SET_GLOBAL:
+        {
+            ObjString *name = READ_STRING();
+            if (tableSet(&vm.globals, name, peek(0)))
+            {
+                tableDelete(&vm.globals, name);
+                runtimeError("Undefined variable '%s'.", name->chars);
+                return INTERPRET_RUNTIME_ERROR;
+            }
             break;
         }
         case OP_EQUAL:
